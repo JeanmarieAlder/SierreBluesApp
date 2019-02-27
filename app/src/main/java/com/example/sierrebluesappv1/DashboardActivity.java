1d678821f098477c3bd9e5a930dcc35db35da1de
Jean-Marie Alder <jeanmarie.alder@gmail.com>
2019-02-27 13:48:37 +0100

    Acts and Stages pages statically filled, background image resized

---
+++ app/src/main/java/com/example/sierrebluesappv1/DashboardActivity.java
@@ -0,0 +1,58 @@
+package com.example.sierrebluesappv1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        Intent intent = getIntent();
+
+        switch (item.getItemId())
+        {
+            case R.id.action_about:
+                intent = new Intent(DashboardActivity.this, AboutActivity.class);
+                DashboardActivity.this.startActivity(intent);
+                break;
+
+            case R.id.action_settings:
+                intent = new Intent(DashboardActivity.this, SettingsActivity.class);
+                DashboardActivity.this.startActivity(intent);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+    public void actsSelected(View view)
+    {
+        Intent intent = new Intent(this, ActsActivity.class);
+        startActivity(intent);
+    }
+
+    public void scenesSelected(View view)
+    {
+        Intent intent = new Intent(this, ScenesActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/sierrebluesappv1/WelcomeActivity.java
@@ -0,0 +1,52 @@
+package com.example.sierrebluesappv1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        Intent intent = getIntent();
+
+        switch (item.getItemId())
+        {
+            case R.id.action_about:
+                intent = new Intent(WelcomeActivity.this, AboutActivity.class);
+                WelcomeActivity.this.startActivity(intent);
+                break;
+
+            case R.id.action_settings:
+                intent = new Intent(WelcomeActivity.this, SettingsActivity.class);
+                WelcomeActivity.this.startActivity(intent);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+    public void timetableSelected(View view)
+    {
+        Intent intent = new Intent(this, TimetableActivity.class);
+        startActivity(intent);
+    }
+}
